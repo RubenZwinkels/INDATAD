78c15b1ec4fcf88991e919d3eee6f34b3adef105
RubenZwinkels <s1149334@student.hsleiden.nl>
2024-09-06 13:31:02 +0200

    [feat] load api key from json

--- tedscraper/tedScraper/api_caller.py
+++ tedscraper/tedScraper/api_caller.py
@@ -1,5 +1,11 @@
 import requests
+import json
 
-def load_captions():
+def get_api_key():
+    filename = "credentials.json"
+    with open(filename, 'r') as file:
+        config = json.load(file)
+    return config.get('api_key')
+def load_captions(video_id):
     # respone = requests.get("https://www.googleapis.com/youtube/v3", "key"="AIzaSyDYRQgz1fzxZDuLxemKU03BjiuZR68jub4")
     pass
