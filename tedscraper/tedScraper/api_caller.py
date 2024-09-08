827530c7a421d294cab78ef304e231ed58189b8c
RubenZwinkels <s1149334@student.hsleiden.nl>
2024-09-08 14:32:55 +0200

    [feat] haalt correcte sub id op

--- tedscraper/tedScraper/api_caller.py
+++ tedscraper/tedScraper/api_caller.py
@@ -10,14 +10,32 @@ def get_api_key():
     return config.get('api_key')
 
 def get_video_captions(video_id):
-    youtube = build("youtube", "v3", developerKey= get_api_key())
+    get_captions_id(video_id)
+
+
+def get_captions_by_id(caption_id):
+    pass
+
+def get_captions_id(video_id):
+    youtube = build("youtube", "v3", developerKey=get_api_key())
     request = youtube.captions().list(
-        part = "snippet",
-        videoId = video_id
+        part="snippet",
+        videoId=video_id
     )
     response = request.execute()
-    print(json.dumps(response, sort_keys=True, indent=4))
+    # print(json.dumps(response, sort_keys=True, indent=4))
 
+    subtitles = response.get("items")
+    correct_sub = None
+    for subtitle in subtitles:
+        if subtitle.get("snippet").get("language") == "en":
+            correct_sub = subtitle
+
+    if correct_sub == None:
+        print(f"geen subtitles gevonden voor id: {video_id}")
+        return None
+    else:
+        return correct_sub
 
 def get_video_metadata(video_id):
     # Bouw de YouTube API service
