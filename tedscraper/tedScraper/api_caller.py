940634fbf660e3493c6145711ce9d903f2a1d993
RubenZwinkels <s1149334@student.hsleiden.nl>
2024-09-13 10:28:08 +0200

    [feat] toppie nieuwe erd en voegt video_data in db

--- tedscraper/tedScraper/api_caller.py
+++ tedscraper/tedScraper/api_caller.py
@@ -1,7 +1,9 @@
-import requests
 import json
-import googleapiclient
 from googleapiclient.discovery import build
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import TextFormatter
+import re
+
 
 def get_api_key():
     filename = "credentials.json"
@@ -9,69 +11,55 @@ def get_api_key():
         config = json.load(file)
     return config.get('api_key')
 
-def get_video_captions(video_id):
-    caption_id = get_captions_id(video_id)
-    get_captions_by_id(caption_id)
 
-def get_captions_by_id(caption_id):
-    youtube = build("youtube", "v3", developerKey=get_api_key())
-    request = youtube.captions().download(
-        id=caption_id
-    )
-    response = request.execute()
-    print(response)
+def clean_transcript(dirty_transcript):
+    cleaned_transcript = re.sub(r'\[.*?\]', '', dirty_transcript)
+    cleaned_transcript = re.sub(r'\b[a-zA-Z]\b', '', cleaned_transcript)
+    cleaned_transcript = re.sub(r'\s+', ' ', cleaned_transcript).strip()
+    return cleaned_transcript
 
-def get_captions_id(video_id):
-    youtube = build("youtube", "v3", developerKey=get_api_key())
-    request = youtube.captions().list(
-        part="snippet",
-        videoId=video_id
-    )
-    response = request.execute()
 
-    subtitles = response.get("items")
-    correct_sub = None
-    for subtitle in subtitles:
-        if subtitle.get("snippet").get("language") == "en":
-            correct_sub = subtitle
+def get_video_data(video_id):
+    try:
+        youtube = build("youtube", "v3", developerKey=get_api_key())
+        request = youtube.videos().list(part="snippet,statistics", id=video_id)
+        response = request.execute()
 
-    if correct_sub == None:
-        print(f"geen subtitles gevonden voor id: {video_id}")
+        if response["items"]:
+            video_info = response["items"][0]
+            title = video_info["snippet"]["title"]
+            description = video_info["snippet"]["description"]
+            views = video_info["statistics"]["viewCount"]
+            likes = video_info["statistics"].get("likeCount", None)
+
+            return {
+                "video_id": video_id,
+                "title": title
+            }
+        else:
+            return {"error": "Video niet gevonden of niet beschikbaar."}
+
+    except Exception as e:
+        print(f"Fout bij het ophalen van metadata: {e}")
         return None
-    else:
-        return correct_sub
 
 
-def get_video_metadata(video_id):
-    # Bouw de YouTube API service
-    youtube = build("youtube", "v3", developerKey=get_api_key())
+def get_video_captions(video_id):
+    try:
+        unformatted_transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        formatter = TextFormatter()
+        formatted_transcript = formatter.format_transcript(unformatted_transcript)
+        cleaned_transcript = clean_transcript(formatted_transcript)
+        return cleaned_transcript
+    except Exception as e:
+        print(f"Fout bij het ophalen van ondertitels: {e}")
+        return None
 
-    # Maak een request om video-informatie op te halen
-    request = youtube.videos().list(
-        part="snippet,statistics",
-        id=video_id  # Video ID van de gewenste video
-    )
 
-    # Voer de request uit
-    response = request.execute()
+def get_video_info(video_id):
+    metadata = get_video_data(video_id)
+    transcript = get_video_captions(video_id)
 
-    # Controleer of er items zijn in de response
-    if response["items"]:
-        video_info = response["items"][0]
-        # Haal de nodige metadata op
-        title = video_info["snippet"]["title"]
-        description = video_info["snippet"]["description"]
-        views = video_info["statistics"]["viewCount"]
-        likes = video_info["statistics"].get("likeCount", None)  # Gebruik None als geen likes beschikbaar zijn
-
-        # Bouw en retourneer een nette dict met de metadata
-        return {
-            "video_id": video_id,
-            "title": title,
-            "description": description,
-            "views": int(views),
-            "likes": int(likes) if likes is not None else None  # Controleer of likes niet None is voordat je het omzet naar een int
-        }
-    else:
-        # Retourneer een lege dict of een melding als de video niet gevonden is
-        return {"error": "Video niet gevonden of niet beschikbaar."}
+    if "error" not in metadata:
+        metadata['transcript'] = transcript
+    return metadata
--- tedscraper/tedScraper/db_writer.py
+++ tedscraper/tedScraper/db_writer.py
@@ -1,30 +1,6 @@
 import psycopg2
 
-def create_table():
-    conn = psycopg2.connect(
-        database = "dataadvanced",
-        user = "postgres",
-        password = "postgres",
-        host = "localhost",
-        port = 5432
-    )
-    cur = conn.cursor()
-
-    query = """
-    CREATE TABLE IF NOT EXISTS video_statistics (
-    video_id VARCHAR(255) NOT NULL,
-    record_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    views INT NOT NULL,
-    likes INT NOT NULL,
-    captions VARCHAR,  -- Optioneel: je kunt een lengte specificeren, zoals VARCHAR(500)
-    PRIMARY KEY (video_id)
-);
-    """
-    cur.execute(query)
-    conn.commit()
-    cur.close()
-
-def insert_video_into_db(statistics):
+def create_connection():
     conn = psycopg2.connect(
         database="dataadvanced",
         user="postgres",
@@ -32,30 +8,94 @@ def insert_video_into_db(statistics):
         host="localhost",
         port=5432
     )
+
+    return conn
+
+def create_table():
+    conn = create_connection()
+    cur = conn.cursor()
+
+    query = """
+ CREATE TABLE IF NOT EXISTS sentiment (
+    id SERIAL PRIMARY KEY,
+    rating DOUBLE PRECISION
+);
+
+CREATE TABLE IF NOT EXISTS popularity (
+    id SERIAL PRIMARY KEY,
+    rating DOUBLE PRECISION
+);
+
+CREATE TABLE IF NOT EXISTS date (
+    id SERIAL PRIMARY KEY,
+    date DATE UNIQUE DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE IF NOT EXISTS video_data(
+    video_id VARCHAR(20) PRIMARY KEY,
+    title VARCHAR(255),
+    transcription VARCHAR,
+    sentiment INT,
+    CONSTRAINT fk_sentiment FOREIGN KEY (sentiment) REFERENCES sentiment(id)
+);
+
+CREATE TABLE IF NOT EXISTS statistic (
+    id SERIAL PRIMARY KEY,
+    video_id VARCHAR(20),  -- Aangepast naar VARCHAR(20) om overeen te komen met video_data
+    current_likes BIGINT,
+    historic_likes BIGINT,
+    current_views BIGINT,
+    historic_views BIGINT,
+    popularity INT,
+    date INT,
+    CONSTRAINT fk_video FOREIGN KEY (video_id) REFERENCES video_data(video_id),
+    CONSTRAINT fk_popularity FOREIGN KEY (popularity) REFERENCES popularity(id),
+    CONSTRAINT fk_date FOREIGN KEY (date) REFERENCES date(id)
+);
+    """
+    cur.execute(query)
+    conn.commit()
+    cur.close()
+
+def insert_video_data_into_db(video_data):
+    conn = create_connection()
     cur = conn.cursor()
 
     insert_query = """
-        INSERT INTO video_statistics (video_id, record_date, views, likes)
-        VALUES (%s, CURRENT_TIMESTAMP, %s, %s)
+        INSERT INTO video_data (video_id, title, transcription)
+        VALUES (%s, %s, %s)
         ON CONFLICT (video_id) DO NOTHING;
         """
 
     try:
         # Voer de query uit met data uit de video_metadata dictionary
         cur.execute(insert_query, (
-            statistics["video_id"],
-            statistics["views"],
-            statistics["likes"]
+            video_data["video_id"],
+            video_data["title"],
+            video_data["transcript"]
         ))
-
-        # Sla de veranderingen op in de database
         conn.commit()
 
         print("Video metadata succesvol ingevoegd.")
     except Exception as e:
-        # Rollback bij fouten
         conn.rollback()
         print(f"Fout bij het invoegen van video metadata: {e}")
     finally:
-        # Sluit de cursor
+        cur.close()
+
+def drop_tables():
+    conn = create_connection()
+    cur = conn.cursor()
+
+    query = """
+     DROP TABLE IF EXISTS statistic, video_data, sentiment, popularity, date CASCADE;
+    """
+
+    try:
+        cur.execute(query)
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        print(f"fout bij het droppen van alle tables: {e}")
+    finally:
         cur.close()
