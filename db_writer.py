da81367892e51f34a2dc0c9d64b1fb2cbc200968
RubenZwinkels <s1149334@student.hsleiden.nl>
2024-09-24 10:37:50 +0200

    update db indeling enzo (historie beter)

--- db_writer.py
+++ db_writer.py
@@ -5,62 +5,60 @@ import os
 from dotenv import load_dotenv
 from db_conn import create_connection
 from datetime import datetime
-from psycopg2 import Error
 
 def create_table():
     conn = create_connection()
     cur = conn.cursor()
 
     query = """
- CREATE TABLE IF NOT EXISTS sentiment (
-    id SERIAL PRIMARY KEY,
-    rating DOUBLE PRECISION
-);
+    CREATE TABLE IF NOT EXISTS sentiment (
+        id SERIAL PRIMARY KEY,
+        rating DOUBLE PRECISION
+    );
 
-CREATE TABLE IF NOT EXISTS popularity (
-    id SERIAL PRIMARY KEY,
-    rating DOUBLE PRECISION
-);
+    CREATE TABLE IF NOT EXISTS popularity (
+        id SERIAL PRIMARY KEY,
+        rating DOUBLE PRECISION
+    );
 
-CREATE TABLE IF NOT EXISTS date (
-    id SERIAL PRIMARY KEY,
-    date DATE UNIQUE DEFAULT CURRENT_DATE
-);
+    CREATE TABLE IF NOT EXISTS date (
+        id SERIAL PRIMARY KEY,
+        date DATE UNIQUE DEFAULT CURRENT_DATE
+    );
 
-CREATE TABLE IF NOT EXISTS video_data(
-    video_id VARCHAR(20) PRIMARY KEY,
-    title VARCHAR(255),
-    transcription VARCHAR,
-    sentiment INT,
-    date INT,
-    CONSTRAINT fk_date FOREIGN KEY (date) REFERENCES date(id),
-    CONSTRAINT fk_sentiment FOREIGN KEY (sentiment) REFERENCES sentiment(id)
-);
+    CREATE TABLE IF NOT EXISTS video_data(
+        video_id VARCHAR(20) PRIMARY KEY,
+        title VARCHAR(255),
+        transcription VARCHAR,
+        sentiment INT,
+        date INT,
+        CONSTRAINT fk_date FOREIGN KEY (date) REFERENCES date(id),
+        CONSTRAINT fk_sentiment FOREIGN KEY (sentiment) REFERENCES sentiment(id)
+    );
 
-CREATE TABLE IF NOT EXISTS statistic (
-    id SERIAL PRIMARY KEY,
-    video_id VARCHAR(20), 
-    current_likes BIGINT,
-    historic_likes BIGINT,
-    current_views BIGINT,
-    historic_views BIGINT,
-    popularity INT,
-    date INT,
-    CONSTRAINT fk_video FOREIGN KEY (video_id) REFERENCES video_data(video_id),
-    CONSTRAINT fk_popularity FOREIGN KEY (popularity) REFERENCES popularity(id),
-    CONSTRAINT fk_date FOREIGN KEY (date) REFERENCES date(id)
-);
+    CREATE TABLE IF NOT EXISTS statistic (
+        id SERIAL PRIMARY KEY,
+        video_id VARCHAR(20), 
+        likes BIGINT,
+        views BIGINT,
+        popularity INT,
+        date INT,
+        CONSTRAINT fk_video FOREIGN KEY (video_id) REFERENCES video_data(video_id),
+        CONSTRAINT fk_popularity FOREIGN KEY (popularity) REFERENCES popularity(id),
+        CONSTRAINT fk_date FOREIGN KEY (date) REFERENCES date(id)
+    );
 
-CREATE TABLE IF NOT EXISTS deploy (
-    id SERIAL PRIMARY KEY,
-    time TIMESTAMP DEFAULT NOW(),
-    script_duration_in_s INTEGER,
-    host VARCHAR
-);
+    CREATE TABLE IF NOT EXISTS deploy (
+        id SERIAL PRIMARY KEY,
+        time TIMESTAMP DEFAULT NOW(),
+        script_duration_in_s INTEGER,
+        host VARCHAR
+    );
     """
     cur.execute(query)
     conn.commit()
     cur.close()
+
 
 def insert_video_data_into_db(video_data):
     conn = create_connection()
@@ -86,6 +84,7 @@ def insert_video_data_into_db(video_data):
     finally:
         cur.close()
 
+
 def drop_tables():
     conn = create_connection()
     cur = conn.cursor()
@@ -103,48 +102,35 @@ def drop_tables():
     finally:
         cur.close()
 
+
 def update_video_statistic(video_id):
     try:
         conn = create_connection()
         cur = conn.cursor()
 
-        old_video_statistic = db_reader.get_video_statistic(video_id)
         new_video_statistic = api_caller.get_video_statistic(video_id)
-        delete_video_statistic(video_id)
+
         query = """
         INSERT INTO statistic
-        (video_id, current_likes, historic_likes, current_views, historic_views, date)
-        VALUES (%s, %s, %s, %s, %s, %s)
+        (video_id, likes, views, date)
+        VALUES (%s, %s, %s, %s)
         """
 
         params = (
             video_id,
             new_video_statistic["likes"],
-            old_video_statistic["current_likes"],
             new_video_statistic["views"],
-            old_video_statistic["current_views"],
             get_date_id_by_date()
         )
         cur.execute(query, params)
         conn.commit()
 
-    except:
-        print(f"fout met video id: {video_id}")
+    except Exception as e:
+        print(f"Fout met video id: {video_id}: {e}")
     finally:
-
         cur.close()
         conn.close()
 
-def delete_video_statistic(video_id):
-    conn = create_connection()
-    cur = conn.cursor()
-    query = f"""
-    DELETE FROM statistic WHERE video_id = '{video_id}';
-    """
-    cur.execute(query)
-    conn.commit()
-    cur.close()
-    conn.close()
 
 def insert_custom_date(custom_date=None):
     try:
@@ -155,9 +141,7 @@ def insert_custom_date(custom_date=None):
             custom_date = datetime.now().date()
 
         if isinstance(custom_date, str):
-            # Verwijder de 'Z' aan het einde van de tijdstring
             custom_date = custom_date.rstrip('Z')
-            # Parse de datumstring zonder 'Z'
             custom_date = datetime.strptime(custom_date, "%Y-%m-%dT%H:%M:%S").date()
 
         query = """
@@ -175,7 +159,7 @@ def insert_custom_date(custom_date=None):
         return inserted_id
 
     except Exception as e:
-        print(f"datum al bestaand in db")
+        print(f"Datum al bestaand in db: {e}")
         return None
 
     finally:
@@ -195,6 +179,7 @@ def get_date_id_by_date(date=None):
         return new_id
     else:
         return excisting_id
+
 
 def insert_deploy(duration):
     load_dotenv()
