238f38274f607c17c41da47ccb4441dd6d9c6ce7
RubenZwinkels <s1149334@student.hsleiden.nl>
2024-10-14 18:15:03 +0200

    [feat] functie om het sentiment te bepalen en ontvangen

---
+++ sentiment/sentiment_rater.py
@@ -0,0 +1,40 @@
+import joblib
+import numpy as np
+
+
+def determine_sentiment(text):
+    # model en vectorizer laden
+    model = load_model()
+    vectorizer = load_vectorizer()
+
+    # tekst omzetten naar een TF-IDF matrix met de geladen vectorizer
+    text_vector = vectorizer.transform([text])  # zorg voor een 2d-array met TF-IDF waarden
+
+    # sentiment voorspellen op basis van de getransformeerde tekst
+    sentiment_rating = model.predict(text_vector)
+
+    print(f"beoordeling: {sentiment_rating[0]}")
+    return sentiment_rating[0]
+
+
+def load_model():
+    try:
+        # als het wordt uitgelezen uit folder sentiment
+        model = joblib.load("classificatie_model.pkl")
+    except FileNotFoundError:
+        # als het wordt uitgelezen uit main
+        model = joblib.load("./sentiment/classificatie_model.pkl")
+    return model
+
+def load_vectorizer():
+    try:
+        # als het wordt uitgelezen uit folder sentiment
+        vectorizer = joblib.load("nlp_model.pkl")
+    except FileNotFoundError:
+        # als het wordt uitgelezen uit main
+        vectorizer = joblib.load("./sentiment/nlp_model.pkl")
+    return vectorizer
+
+if __name__ == "__main__":
+    test_text = "A Palestinian and an Israeli, Face to Face | Aziz Abu Sarah and Maoz Inon | TED"
+    determine_sentiment(test_text)
